e19645240b665636878658a689e6ddcb05368811
Rouwel <rouwelngacha@gmail.com>
2025-12-03 10:35:49 +0300

    Fourth commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import UserRegisterView
-
-
+from .views import views.register 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
