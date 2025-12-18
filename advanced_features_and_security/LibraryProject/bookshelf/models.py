a0125de9a2ae12e4282880bd478bf8db036a26f8
Rouwel <rouwelngacha@gmail.com>
2025-12-18 19:29:16 +0300

    Creation of Custom user model complete : 2

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
 
 class Book(models.Model):
@@ -13,12 +14,31 @@ class Book(models.Model):
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null = True, blank =True)
-    profile_photo = models.ImageField(upload_to= 'profile_photos/', null = False, blank = False)
+    profile_photo = models.ImageField(upload_to= 'profile_photos/', null = True, blank = True)
     
     def __str__(self):
         return f"{self.profile_photo}"
     
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, date_of_birth = None, profile_photo = None):
+        user = self.model(username = username, email = email, date_of_birth = date_of_birth, profile_photo = profile_photo)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
     
+    def create_superuser(self, username, email, date_of_birth = None, profile_photo = None):
+        extra_fields.setdefault('is_staff', True) 
+        extra_fields.setdefault('is_superuser', True) 
+        return self.create_user(username, email, password=password,)
+        
+
+        
+
+
+
+
+    
+
 
 
 
