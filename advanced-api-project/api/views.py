fef303c00a67ebc375f12164cfc690864d4f783e
Rouwel <rouwelngacha@gmail.com>
2026-01-05 22:50:03 +0300

    Checker not finding imports

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from .serializers import BookSerializer,AuthorSerializer
 from .models import Book, Author
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend, filters, OrderingFilter
+from django_filters import rest_framework
 
 # Create your views here.
 
