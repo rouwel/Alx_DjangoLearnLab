73b092337396f72dd4518656dfa48f43571acd74
Rouwel <rouwelngacha@gmail.com>
2026-01-12 22:52:16 +0300

    Addition of more settings settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -115,9 +115,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR, "static",
+    BASE_DIR/ "blog/static",
 ]
+
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
 
 
