ae93fd091e795d4830d2e815bd5978e18ffcbf45
Rouwel <rouwelngacha@gmail.com>
2026-01-13 11:20:34 +0300

    Addition of code to views in order to allow for post manipulation

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Post
+from taggit.forms import TagW
+
 
 
 class UserCreateForm(UserCreationForm):
@@ -16,3 +19,9 @@ class UserUpdaterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content'] 
+        
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import PostCreateView, PostDeleteView, PostDetailView,PostUpdateView, PostListView
+
 
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="blog/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
-    path("profile/", views.profile, name="profile")
+    path("profile/", views.profile, name="profile"),
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('post/new/', PostCreateView.as_view(), name='post-creaion'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-details'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete')
 ]
+
